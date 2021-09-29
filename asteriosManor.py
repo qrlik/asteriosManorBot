837cad3741e043c2d4dd1e17dbf0964a4fc44ebe
qrlik <qrlikk@gmail.com>
2021-09-30 01:08:36 +0300

    fix paused and stop, fix scroll

--- asteriosManor.py
+++ asteriosManor.py
@@ -29,15 +29,9 @@ def __getGlobalPoint(x, y):
     global __monitor
     return (x + __monitor['left'], y + __monitor['top'])
 
-def __checkPause():
-    while __paused:
-        time.sleep(3)
-    return
-
 def __openChatWindow():
     global __autoPy
     global __delay
-    __checkPause()
     __autoPy.F1.press()
     __autoPy.F1.press()
     time.sleep(__delay)
@@ -48,60 +42,43 @@ def __detectChatWindowScroll():
     global __delay
     with mss.mss() as screenshotManager:
         while True:
-            __checkPause()
             img = numpy.array(screenshotManager.grab(__monitor))
             processedImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
             res = cv2.matchTemplate(processedImage, __chatWindowTemplate, cv2.TM_CCOEFF_NORMED)
             loc = numpy.where(res >= 0.9)
 
             for point in zip(*loc[::-1]):
                 templateHeight = __chatWindowTemplate.shape[0]
                 templateWidth = __chatWindowTemplate.shape[1]
-
-                #cv2.rectangle(img, point, (point[0] + templateWidth, point[1] + templateHeight), (0, 255, 0), 3)
-                #cv2.imshow("test", img)
-                #key = cv2.waitKey(1)
-                    
+                #cv2.rectangle(img, point, (point[0] + templateWidth, point[1] + templateHeight), (0, 255, 0), 3) #debug
                 scrollPoint = __getGlobalPoint(point[0], point[1])
                 scrollMiddlePoint = (int(scrollPoint[0] + templateWidth / 2), int(scrollPoint[1] + templateHeight / 2))
                 return scrollMiddlePoint
+            #cv2.imshow("test", img) #debug
+            #key = cv2.waitKey(1)
             time.sleep(__delay)
 
 def __scrollChatWindow(point):
     global __autoPy
     global __delay
-    __checkPause()
     __autoPy.moveMouseToPosition(point[0], point[1])
     stroke = InterceptionMouseStroke()
     stroke.state = InterceptionMouseState.INTERCEPTION_MOUSE_LEFT_BUTTON_DOWN
     __autoPy.sendToDefaultMouse(stroke)
+    time.sleep(__delay)
     __autoPy.moveMouseToPosition(point[0], point[1] + 295)
     stroke.state = InterceptionMouseState.INTERCEPTION_MOUSE_LEFT_BUTTON_UP
     __autoPy.sendToDefaultMouse(stroke)
     time.sleep(__delay)
 
 def proceed(autohotpy, event):
-    __checkPause()
+    global __paused
     #with pyautogui.hold('shift'):
-
     __openChatWindow()
     scrollPoint = __detectChatWindowScroll()
     __scrollChatWindow(scrollPoint)
-
-    autohotpy.run(proceed, event)
-
-def run():
-    global __autoPy
-    __init()
-    __autoPy.registerExit(__autoPy.END, __onExit)
-    __autoPy.registerForKeyDown(__autoPy.F12, __switchPause)
-    __autoPy.start()
-
-
-def __onExit(autohotpy, event):
-    autohotpy.stop()
-    raise Exception('exit')
+    if not __paused:
+        autohotpy.run(proceed, event)
 
 def __switchPause(autohotpy, event):
     global __paused
@@ -110,12 +87,25 @@ def __switchPause(autohotpy, event):
     if not __paused:
         proceed(autohotpy, event)
 
+def __onExit(autohotpy, event):
+    global __paused
+    __paused = True
+    autohotpy.stop()
+
+def run():
+    global __autoPy
+    __init()
+    __autoPy.registerForKeyDown(__autoPy.F11, __switchPause)
+    __autoPy.registerExit(__autoPy.ESC, __onExit)
+    __autoPy.start()
+
+    
 
 def main():
-    try:
-        run()
-    except:
-        print('Good bye')
+    run()
+
+    #__init() # debug
+    #proceed(None, None)
 
 if __name__ == '__main__':
     main()
